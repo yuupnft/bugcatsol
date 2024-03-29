6bbf3472d0f368ded2d3f00814a41a3ad11d1a8a
yuupnft <165105972+yuupnft@users.noreply.github.com>
2024-03-29 02:29:28 -0500

    Fixed formatting issues on mobile

--- client/pages/index.js
+++ client/pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
             He is a cute house pet who craves meat and despises vegetables, living with his owner Lala and the seemingly immortal Dogdog—a large dog possessing uncanny skills in all fields—in Lalas house.
           </p>
           <div className={styles.infos_shape}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" preserveAspectRatio="none" viewBox="0 0 168.5 152.6" className="" fill="rgba(231, 249, 253, 1)"><path d="M25.1,17.1s69.1-39.5,95.6,1.4S180,30,164.5,90c-12.4,47.8-26.1,20.6-62,57.9-17.8,18.5-77.2-22-89.8-48.3C-8.8,54.6-2,32.2,25.1,17.1Z"></path></svg>
+            <svg className={styles.svg} xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" preserveAspectRatio="none" viewBox="0 0 168.5 152.6" fill="rgba(231, 249, 253, 1)"><path d="M25.1,17.1s69.1-39.5,95.6,1.4S180,30,164.5,90c-12.4,47.8-26.1,20.6-62,57.9-17.8,18.5-77.2-22-89.8-48.3C-8.8,54.6-2,32.2,25.1,17.1Z"></path></svg>
           </div>
         </section>
       </div>
@@ -77,7 +77,7 @@ export default function Home() {
             68hq3Lgqg4a7qHoGpgf7Pz18swuMpPc5gXYmPt3UEZ7L
           </p>
           <div className={styles.infos_shape_2}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" preserveAspectRatio="none" viewBox="0 0 168.5 152.6" className="" fill="rgba(231, 249, 253, 1)"><path d="M25.1,17.1s69.1-39.5,95.6,1.4S180,30,164.5,90c-12.4,47.8-26.1,20.6-62,57.9-17.8,18.5-77.2-22-89.8-48.3C-8.8,54.6-2,32.2,25.1,17.1Z"></path></svg>
+            <svg className={styles.svg_2} xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" preserveAspectRatio="none" viewBox="0 0 168.5 152.6" fill="rgba(231, 249, 253, 1)"><path d="M25.1,17.1s69.1-39.5,95.6,1.4S180,30,164.5,90c-12.4,47.8-26.1,20.6-62,57.9-17.8,18.5-77.2-22-89.8-48.3C-8.8,54.6-2,32.2,25.1,17.1Z"></path></svg>
           </div>
         </section>
       </div>
